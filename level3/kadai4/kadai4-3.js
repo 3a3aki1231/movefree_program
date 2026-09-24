ca0538c7335949ff81d547624980c840689e1ebe
Sasaki <3a3aki1231@gmail.com>
2026-09-25 00:31:50 +0900

    add:仮題4

---
+++ level3/kadai4/kadai4-3.js
@@ -0,0 +1,9 @@
+
+jQuery(document).ready(function($) {
+    alert('kadai4-3.jsが読み込まれました！');
+
+    $('a').hover(
+        function() { $(this).css('opacity', '0.7'); },
+        function() { $(this).css('opacity', '1'); }
+    );
+});
